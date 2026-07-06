32fad55817af61b1c8124c140f6fc8c37ee55a30
mateussantosrodrigues650-hash <mateusantosrodrigues650@gmail.com>
2026-07-06 10:50:24 -0300

    uptade modulo001

---
+++ modulo001/pensamento_projeto_junho_matheussantos.py
@@ -0,0 +1,129 @@
+""""
+>PO (como dono do negocio:quero um aplicativo de vendas para minha Hamburgueria
+para que eu possa controlar as vendas e os produtos.)
+
+>QA (como cliente: quero um sistema de vendas para minha Hamburgueria, 
+para que eu possa comprar meus produtos de forma rapida e facil.)
+
+>Teach (como programador: quero um sistema de vendas para minha Hamburgueria,
+para que eu possa desenvolver um aplicativo eficiente e funcional para o negocio.)
+
+>Dev (como programador:quero um sistema de vendas para minha Hamburgueria,
+para que eu possa implementar as funcionalidades necessarias para atender
+as necessidades do negocio e dos clientes.)
+
+>UX (como designer de experiencia do usario:Quero um sistema de 
+vendas para minha Hamburgueria,para que eu possa criar uma interface
+intuitiva e agradavel para os usuarios, garantindo uma experiencia
+de compra satisfatoria.)
+
+>IA (como analista de dados:Quero um sistema de vendas para minha
+Hamburgueria, para que eu possa coletar e analisar os dados de vendas,
+ajudando a identificar padrões de consumo e otimizar as
+estrategias de marketing e estoque.)
+
+
+
+
+
+
+
+
+
+"""
+
+ Isso e um # comentario de linha unica
+print ('ola, mundo!')
+while True:
+print ('-' * 48 + '\n')
+print ('\n------------------------------------------------\n')
+print ('Bem-vindo ao sistema de vendas - Hamburgueria!\n')
+print ('1 - cadrastar produto')
+print ('2 - Listar produtos')
+print ('3 - realizar venda')
+print ('0 - sair')
+print ('\n -------------------------------------------------------------\n')
+
+opcao = input ('Digite a opção desejada: ')
+
+if opcao == '1':
+   print('opção 1 - Cadastrar produto')
+
+ elif: opçao == '2':# Isso e um comentario de linha unica
+print ('ola, mundo!')
+
+print ('\n------------------------------------------------\n')
+print ('Bem-vindo ao sistema de vendas - Hamburgueria!\n')
+print ('1 - cadrastar produto')
+print ('2 - Listar produtos')
+print ('3 - realizar venda')
+print ('0 - sair')
+print ('\n -------------------------------------------------------------\n')
+
+opcao = input ('Digite a opção desejada: ')
+
+if opcao == '1'
+   print('opção 1 - Cadastrando  produto...\n')
+   if p1_nome == "":
+   p1_nome = input('Digite o nome do produto: ')
+   p1_estoque = int(inputut('Digite a quantidade em estoque: ' ))
+   p1_preco  = float(input('Digite o preço do produto: '))
+   p1_descricao = input('Digite a descricao do produto: ')
+   print()
+
+elif: opçao == '2'opçao:
+   print (' opção 2 - Listar produtos')
+
+elif: opçao == '3':
+   print ('opcão 3 - realizar venda')
+          
+elif: opcao == '0':
+ print ('opção 0 - Sair')
+break
+
+else:
+print ('opção invalida!')
+
+
+
+
+
+
+
+# Isso e um comentario de linha unica
+print ('ola, mundo!')
+
+print ('\n------------------------------------------------\n')
+print ('Bem-vindo ao sistema de vendas - Hamburgueria!\n')
+print ('1 - cadrastar produto')
+print ('2 - Listar produtos')
+print ('3 - realizar venda')
+print ('0 - sair')
+print ('\n -------------------------------------------------------------\n')
+
+opcao = input ('Digite a opção desejada: ')
+
+if opcao == '1'
+   print('opção 1 - Cadastrar produto')
+nome_produto = input ('Digite o nome do produto: ')
+quantidade_estoque = int(input('Digite a quantidade em estoque: ' ))
+preco_produto = float(input('Digite o preço do produto: '))
+descricao_produto = (input('Digite a descriçao do produto: '))
+
+elif opcao == '2':
+print (' opção 2 - Listar produtos')
+cardapio_lanches = input ('mostre o cardapio:') 
+
+
+
+
+
+ elif: opçao == '3':
+   print ('opcão 3 - realizar venda')
+          
+elif: opcao == '0':
+ print ('opção 0 - Sair')
+break
+
+else:
+print ('opção invalida!')
