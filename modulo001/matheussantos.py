0c80fe76e9e292a8df0753509d8af94d945d4444
mateussantosrodrigues650-hash <mateusantosrodrigues650@gmail.com>
2026-07-06 10:48:22 -0300

    uptade modulo001

---
+++ modulo001/matheussantos.py
@@ -0,0 +1,21 @@
+'''
+Bloco de comentarios
+
+Modulo o4 - Estrutura de dados - listas
+'''''''''
+
+Frutas_gostosas = ['laranja' , 'manga', 'abacaxi', 'morango' , 'uva', 'abacate', 'kiwi' , 'pessego' , 'mamao']
+
+print('🍊' * 40)
+
+print('Bem vindo ao sistema de frutas gostosas')
+
+print('🍊' * 40)
+
+frutas_gostosas.append('fruta_escolhida')
+
+
+
+
+print(f'\nfruta adicionada com sucesso! \nA nova lista de frutas gostosas e: 
+      {frutas_gostosas}')
